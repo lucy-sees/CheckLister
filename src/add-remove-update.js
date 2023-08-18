ed43adff2b23bd133e683389ad09761bb56c2814
Lucy W. Mwangi <100368317+lucy-sees@users.noreply.github.com>
2023-08-18 14:53:15 +0300

    Implement updateArray functionality

---
+++ src/add-remove-update.js
@@ -0,0 +1,22 @@
+export const addToArr = (val, arr) => {
+  arr.push({
+    description: val,
+    completed: false,
+    index: arr.length + 1,
+  });
+};
+
+export const reorder = (arr) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    arr[i].index = i + 1;
+  }
+};
+
+export const removeFromArr = (index, arr) => {
+  arr.splice(index - 1, 1);
+  reorder(arr);
+};
+
+export const updateArr = (index, newVal, arr) => {
+  arr[index - 1].description = newVal;
+};
